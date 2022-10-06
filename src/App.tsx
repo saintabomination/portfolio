c326e726683b0256a2ae0624fc0f84fd8674df49
saintabomination <saint.abomination@protonmail.com>
2022-10-06 19:44:35 +0200

    CSS Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Desktop from './components/Desktop.component';
 import Taskbar from './components/Taskbar.component';
 
+import './styles/reset.scss';
+
 const App = (): JSX.Element =>
   (
     <Desktop>
